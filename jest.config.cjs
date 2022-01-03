50bf7ce291f0ee8265e5f054ed17e49dc06aa5e8
Arie Trouw <arie.trouw@xylabs.com>
2022-01-03 11:35:11 -0800

    fixed jest types

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  preset: 'ts-jest/presets/default-esm',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 }

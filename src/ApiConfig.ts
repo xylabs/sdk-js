abb554354c9275f791bbccde001a359d9118236b
Arie Trouw <arie.trouw@xylabs.com>
2022-02-02 09:36:02 -0800

    ApiConfig updated

--- src/ApiConfig.ts
+++ src/ApiConfig.ts
@@ -1,7 +1,10 @@
 interface ApiConfig {
   apiDomain: string
+  /** @deprecated use jwtToken or apiKey instead */
   token?: string
   userid?: string
+  jwtToken?: string
+  apiKey?: string
 }
 
 export type { ApiConfig }

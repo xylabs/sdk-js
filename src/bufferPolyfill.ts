c0133837d3b9f19574185d53c4a735f64de69cb3
Arie Trouw <arie.trouw@xylabs.com>
2022-04-22 10:37:16 -0700

    using safe-buffer

--- src/bufferPolyfill.ts
+++ src/bufferPolyfill.ts
@@ -1,6 +1,7 @@
-import { Buffer } from 'buffer'
+import { Buffer } from 'safe-buffer'
 
 //make sure we have a global Buffer object if in browser
+/** @deprecated use safe-buffer npm package instead */
 export const bufferPolyfill = () => {
   if (window !== undefined) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any

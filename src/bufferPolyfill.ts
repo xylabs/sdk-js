54c1fdc95c4a5afbd56f966355d6bf8c4c3bc5c0
Arie Trouw <arie.trouw@xylabs.com>
2022-04-25 13:44:43 -0700

    undeprecated buffer polyfill

--- src/bufferPolyfill.ts
+++ src/bufferPolyfill.ts
@@ -1,7 +1,5 @@
 import { Buffer } from './Buffer'
 
-//make sure we have a global Buffer object if in browser
-/** @deprecated use Buffer from @xylabs/sdk-js npm package instead */
 export const bufferPolyfill = () => {
   if (window !== undefined) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any

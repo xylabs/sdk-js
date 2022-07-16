4db8c01e0cb2824d17314cd6a6e30257cc06aa14
Arie Trouw <arie.trouw@xylabs.com>
2022-07-16 12:16:39 -0700

    monorepo

--- packages/buffer/src/browserIndex.ts
+++ packages/buffer/src/browserIndex.ts
@@ -1,3 +1,2 @@
 export * from './browser'
 export { BrowserBuffer as Buffer, bufferPolyfillBrowser as bufferPolyfill } from './browser'
-export * from './shared'
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export * from './browser'
-export { BrowserBuffer as Buffer, bufferPolyfillBrowser as bufferPolyfill } from './browser'
-export * from './node'
 export * from './shared'

82b5fffe6bd7b70bc61513c247a824ddaa4a9825
Arie Trouw <arie.trouw@xylabs.com>
2022-09-27 09:55:24 -0700

    deprecated PartialRecord

--- src/shared/PartialRecord.ts
+++ src/shared/PartialRecord.ts
@@ -1,4 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/** @deprecated use Partial<Record<>> instead */
 export type PartialRecord<K extends keyof any, T> = {
   [P in K]?: T
 }

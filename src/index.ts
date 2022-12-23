ddc6a04798c49dbdf2b9c10a2d1721927b96ecf5
Arie Trouw <arie.trouw@xylabs.com>
2022-12-23 12:19:50 -0800

    split out api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export * from './shared'
+export * from './Override'
+export * from './PartialRecord'
+export * from '@xylabs/api'
 export * from '@xylabs/assert'
 export * from '@xylabs/bignumber'
 export * from '@xylabs/delay'

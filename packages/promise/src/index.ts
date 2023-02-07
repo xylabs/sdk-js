72545e56e2302a5064bb09bf517b3f3e811947ef
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-07 06:24:25 -0600

    Expose helper methods

--- packages/promise/src/index.ts
+++ packages/promise/src/index.ts
@@ -1,4 +1,5 @@
 import { fulfilled } from './fulfilled'
+import { fulfilledValues } from './fulfilledValues'
 import { rejected } from './rejected'
 
-export { fulfilled, rejected }
+export { fulfilled, fulfilledValues, rejected }

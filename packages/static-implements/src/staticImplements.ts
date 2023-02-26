66c1bc3ae52ea2129e862fc1b129e2435d25dcf2
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-02-26 16:33:52 -0600

    Export method from project

--- packages/static-implements/src/staticImplements.ts
+++ packages/static-implements/src/staticImplements.ts
@@ -1,8 +1,7 @@
 /**
- * Annotation to be used to decorate classes which support
- * an asynchronous creation pattern
- * @returns The decorated Module requiring it implement the members
- * of the CreatableModule as statics properties/methods
+ * Annotation to decorate classes which implement static methods
+ * @returns The decorated class requiring it to implement
+ * the members of the the type as static properties/methods
  */
 export function staticImplements<T>() {
   return <U extends T>(constructor: U) => {

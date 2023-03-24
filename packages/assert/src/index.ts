09e8e07f9e00abe966f29933f6a398da4414ac0b
Arie Trouw <arie.trouw@xylabs.com>
2023-03-24 14:21:05 -0700

    Support for function in assertEx

--- packages/assert/src/index.ts
+++ packages/assert/src/index.ts
@@ -3,21 +3,23 @@
  */
 class AssertExError extends Error {}
 
+export type AssertExMessageFunc = <T>(value: T) => string
+
 /**
  *
  * Intended for simple null/undefined checks for variables
  *
  * @param expr - Expression to be evaluated for truthiness
- * @param message - Message in Error if expression is false
+ * @param message - Message in Error if expression is false, may be a function that returns a string
  * @throws AssertExError
  * @returns Value of expression
  */
-const assertEx = <T>(expr?: T | null, message?: string): T => {
+const assertEx = <T>(expr?: T | null, message?: string | AssertExMessageFunc): T => {
   if (expr) return expr
-  throw new AssertExError(message)
+  throw new AssertExError(typeof message === 'string' ? message : message?.(expr))
 }
-
-export { assertEx }
 
 // eslint-disable-next-line import/no-default-export
 export default assertEx
+
+export { assertEx }

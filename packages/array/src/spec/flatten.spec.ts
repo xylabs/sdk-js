2be14fb0f9ba21081ea6b643ddee0eca16627964
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-05-17 14:23:21 -0500

    Test for flatten

--- packages/array/src/spec/flatten.spec.ts
+++ packages/array/src/spec/flatten.spec.ts
@@ -1,7 +1,13 @@
 import { flatten } from '../flatten'
 
 describe('flatten', () => {
-  const testCases: [string[], string[], string[]][] = [[['a'], ['b', 'c'], ['a', 'b', 'c']]]
+  const testCases: [unknown | unknown[], unknown | unknown[], unknown | unknown[]][] = [
+    [['a'], ['b', 'c'], ['a', 'b', 'c']],
+    ['a', ['b', 'c'], ['a', 'b', 'c']],
+    [['a', 'b'], undefined, ['a', 'b']],
+    [undefined, undefined, []],
+    [['a'], [1, 2], ['a', 1, 2]],
+  ]
   it.each(testCases)('flattens inputs', (a, b, expected) => {
     expect(flatten(a, b)).toEqual(expected)
   })

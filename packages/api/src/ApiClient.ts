65a26a8dc3a91457aeed5fb179f0c26f19221164
Arie Trouw <arie.trouw@xylabs.com>
2023-09-07 18:02:31 -0700

    deploy 2.10.0

--- packages/api/src/ApiClient.ts
+++ packages/api/src/ApiClient.ts
@@ -1,7 +1,10 @@
 import { ApiStage } from './ApiStage'
 
 abstract class ApiClient {
-  public constructor(protected token?: string | null, protected stage?: ApiStage) {
+  public constructor(
+    protected token?: string | null,
+    protected stage?: ApiStage,
+  ) {
     this.stage = stage ?? ApiStage.Prod
     this.token = token
   }

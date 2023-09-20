cd5c796ba85a49bd51cb5ccea971a6ac1edf9c72
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-09-20 08:43:12 -0500

    More tests cases

--- packages/set/src/lib/spec/difference.spec.ts
+++ packages/set/src/lib/spec/difference.spec.ts
@@ -1,6 +1,12 @@
 import { difference } from '../difference'
 
-const cases = [[new Set([1, 2, 3]), new Set([2, 3, 4]), new Set([1])]]
+const cases = [
+  // Difference is commutative
+  [new Set([1, 2, 3]), new Set([2, 3, 4]), new Set([1])],
+  [new Set([2, 3, 4]), new Set([1, 2, 3]), new Set([4])],
+  // No difference yields empty set
+  [new Set([1, 2, 3]), new Set([1, 2, 3]), new Set([])],
+]
 
 describe('difference', () => {
   test.each(cases)('calculates the difference', (a, b, expected) => {

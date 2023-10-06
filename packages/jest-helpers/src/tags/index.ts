29ec22454e729e77c72efa5036db7b383ab876a2
Arie Trouw <arie.trouw@xylabs.com>
2023-10-06 13:25:02 -0700

    deploy 2.12.20

--- packages/jest-helpers/src/tags/index.ts
+++ packages/jest-helpers/src/tags/index.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const argv = require('minimist')(process.argv.slice(2))
-
 import { describeIf } from '../describeIf'
 import { itIf } from '../itIf'
 import { testIf } from '../testIf'
@@ -9,7 +6,9 @@ import { matchFilter } from './filter'
 //Based on https://www.npmjs.com/package/jest-tags/v/1.0.1
 
 export function tags(...tagLabels: string[]) {
-  const filter = matchFilter(argv.tags)
+  const tagsParam = process.argv.findIndex((item) => item === '--tags')
+  const passedTags = tagsParam > 0 ? process.argv[tagsParam + 1] : undefined
+  const filter = matchFilter(passedTags)
   const thisGlobal = global
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -17,9 +16,12 @@ export function tags(...tagLabels: string[]) {
 
   const filterMatch = filter(tagLabels)
 
-  chain.describeIf = filterMatch ? describeIf : thisGlobal.xdescribe
-  chain.itIf = filterMatch ? itIf : thisGlobal.xit
-  chain.testIf = filterMatch ? testIf : thisGlobal.xtest
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  chain.describeIf = filterMatch ? describeIf : (_param: any) => describe.skip
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  chain.itIf = filterMatch ? itIf : (_param: any) => it.skip
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  chain.testIf = filterMatch ? testIf : (_param: any) => test.skip
   chain.describe = filterMatch ? thisGlobal.describe : thisGlobal.xdescribe
   chain.it = filterMatch ? thisGlobal.it : thisGlobal.xit
   chain.test = filterMatch ? thisGlobal.test : thisGlobal.xtest

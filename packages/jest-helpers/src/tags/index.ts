9243f0e23b72c4d449381aacfddf3e414df09578
Arie Trouw <arie.trouw@xylabs.com>
2023-10-06 12:32:39 -0700

    deploy 2.12.19

--- packages/jest-helpers/src/tags/index.ts
+++ packages/jest-helpers/src/tags/index.ts
@@ -2,6 +2,8 @@
 const argv = require('minimist')(process.argv.slice(2))
 
 import { describeIf } from '../describeIf'
+import { itIf } from '../itIf'
+import { testIf } from '../testIf'
 import { matchFilter } from './filter'
 
 //Based on https://www.npmjs.com/package/jest-tags/v/1.0.1
@@ -15,13 +17,16 @@ export function tags(...tagLabels: string[]) {
 
   const filterMatch = filter(tagLabels)
 
-  chain.describeIf = describeIf
-  chain.describe = thisGlobal.describe
-  chain.it = thisGlobal.it
+  chain.describeIf = filterMatch ? describeIf : thisGlobal.xdescribe
+  chain.itIf = filterMatch ? itIf : thisGlobal.xit
+  chain.testIf = filterMatch ? testIf : thisGlobal.xtest
+  chain.describe = filterMatch ? thisGlobal.describe : thisGlobal.xdescribe
+  chain.it = filterMatch ? thisGlobal.it : thisGlobal.xit
   chain.test = filterMatch ? thisGlobal.test : thisGlobal.xtest
   chain.test.only = thisGlobal.test.only
   chain.test.skip = thisGlobal.test.skip
   chain.xtest = thisGlobal.xtest
+  chain.xit = thisGlobal.xit
 
   return chain
 }

eba35ef1287a6489b702470a78bb7571629d5f7f
Arie Trouw <arie.trouw@xylabs.com>
2023-10-16 17:38:58 -0700

    deploy 2.12.29

--- packages/platform/src/browser/index.ts
+++ packages/platform/src/browser/index.ts
@@ -1,3 +1,5 @@
+import type { subtle as subtleType } from 'crypto'
+
 export const isBrowser = () => {
   return typeof window !== 'undefined' && typeof window?.document !== 'undefined'
 }
@@ -7,3 +9,6 @@ export const isWebworker = () => {
 }
 
 export const getGlobal = <T extends object>() => (isBrowser() ? (window as unknown as T) : isWebworker() ? (self as unknown as T) : globalThis)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const subtle = (getGlobal() as any).subtle as typeof subtleType
---
+++ packages/platform/src/node/index.ts
@@ -0,0 +1,11 @@
+export { subtle } from 'crypto'
+
+export const isBrowser = () => {
+  return typeof window !== 'undefined' && typeof window?.document !== 'undefined'
+}
+
+export const isWebworker = () => {
+  return typeof self === 'object' && self.constructor?.name === 'DedicatedWorkerGlobalScope'
+}
+
+export const getGlobal = <T extends object>() => (isBrowser() ? (window as unknown as T) : isWebworker() ? (self as unknown as T) : globalThis)

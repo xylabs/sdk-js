4f615c849c5fd7a46cedd0836738f5aabea473b9
Arie Trouw <arie.trouw@xylabs.com>
2023-10-16 18:10:24 -0700

    PromiseEx

--- packages/promise/src/index.ts
+++ packages/promise/src/index.ts
@@ -3,3 +3,5 @@ import { fulfilledValues } from './fulfilledValues'
 import { rejected } from './rejected'
 
 export { fulfilled, fulfilledValues, rejected }
+export * from './PromiseEx'
+export * from './types'
---
+++ packages/promise/src/types.ts
@@ -0,0 +1,11 @@
+import { PromiseEx } from './PromiseEx'
+
+export type Promisable<T, V = never> = PromiseEx<T, V> | Promise<T> | T
+export type PromisableArray<T, V = never> = Promisable<T[], V>
+export type OptionalPromisable<T, V = never> = Promisable<T | undefined, V>
+export type OptionalPromisableArray<T, V = never> = PromisableArray<T | undefined, V>
+export type NullablePromisable<T, V = never> = Promisable<T | null, V>
+export type NullablePromisableArray<T, V = never> = PromisableArray<T | null, V>
+
+/** @description Used to document promises that are being used as Mutexes */
+export type AsyncMutex<T> = Promise<T>

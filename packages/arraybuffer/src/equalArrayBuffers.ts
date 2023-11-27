0af282e4d2f011297ae4e870096aa655fdee65e1
Arie Trouw <arie.trouw@xylabs.com>
2023-11-27 15:56:07 -0800

    deploy 2.13.18

---
+++ packages/arraybuffer/src/equalArrayBuffers.ts
@@ -0,0 +1,9 @@
+export const equalArrayBuffers = (a1: ArrayBuffer, a2: ArrayBuffer) => {
+  const u1 = new Uint8Array(a1)
+  const u2 = new Uint8Array(a2)
+  if (u1.byteLength !== u2.byteLength) return false
+  for (let i = 0; i < u1.byteLength; i++) {
+    if (u1[i] !== u2[i]) return false
+  }
+  return true
+}
--- packages/arraybuffer/src/index.ts
+++ packages/arraybuffer/src/index.ts
@@ -1,3 +1,4 @@
+export * from './equalArrayBuffers'
 export * from './toArrayBuffer'
 export * from './toUint8Array'
 export { isArrayBuffer } from '@xylabs/lodash'

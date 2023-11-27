e244bfc8c5086b95be76dd52d49066fa38fde051
Arie Trouw <arie.trouw@xylabs.com>
2023-11-27 13:36:49 -0800

    deploy 2.13.17

--- packages/hex/src/hex/is.ts
+++ packages/hex/src/hex/is.ts
@@ -11,8 +11,6 @@ export const isHex = (value: unknown, bitLength?: number): value is Hex => {
   //Is it a string?
   if (typeof value !== 'string') return false
 
-  console.log(`BitLength: ${value.length << 2}|${bitLength}`)
-
   //If a bitLength specified, does it conform?
   if (bitLength !== undefined && value.length !== bitsToNibbles(bitLength)) return false
 

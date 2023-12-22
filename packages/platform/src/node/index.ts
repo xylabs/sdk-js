530f9c2e6beb040721d1fb995c9b550257d45fbe
Arie Trouw <arie.trouw@xylabs.com>
2023-12-22 11:03:47 -0800

    fixed isBrowser on node - 2.13.22

--- packages/platform/src/node/index.ts
+++ packages/platform/src/node/index.ts
@@ -1,11 +1,11 @@
 export { subtle } from 'node:crypto'
 
 export const isBrowser = () => {
-  return window?.document !== undefined
+  return false
 }
 
 export const isWebworker = () => {
-  return typeof self === 'object' && self.constructor?.name === 'DedicatedWorkerGlobalScope'
+  return false
 }
 
 export const getGlobal = <T extends object>() => (isBrowser() ? (window as unknown as T) : isWebworker() ? (self as unknown as T) : globalThis)

9255d5a38364345a74b70c77747a1c7c9a9d7369
Arie Trouw <arie.trouw@xylabs.com>
2023-12-26 14:51:09 -0800

    Added ConsoleLogger

--- packages/logger/src/Logger.ts
+++ packages/logger/src/Logger.ts
@@ -14,6 +14,39 @@ export interface Logger {
   warn: LogFunction
 }
 
+export enum LogLevel {
+  error = 1,
+  warn = 2,
+  info = 3,
+  debug = 4,
+  log = 5,
+}
+
+export const NoOpLogFunction = (_message?: unknown) => void {}
+
+export class ConsoleLogger implements Logger {
+  constructor(readonly level: LogLevel = LogLevel.warn) {}
+
+  get debug() {
+    return this.level <= LogLevel.debug ? console.debug : NoOpLogFunction
+  }
+
+  get error() {
+    return this.level <= LogLevel.error ? console.error : NoOpLogFunction
+  }
+
+  get info() {
+    return this.level <= LogLevel.info ? console.info : NoOpLogFunction
+  }
+
+  get log() {
+    return this.level <= LogLevel.log ? console.log : NoOpLogFunction
+  }
+
+  get warn() {
+    return this.level <= LogLevel.warn ? console.warn : NoOpLogFunction
+  }
+}
 export const getFunctionName = (depth = 2) => {
   try {
     throw new Error('Getting function name')

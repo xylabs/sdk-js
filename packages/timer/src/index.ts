14319d00a90779c5ed54067a8b1a77b924f6b20d
Arie Trouw <arie.trouw@xylabs.com>
2024-01-18 14:39:31 -0800

    clearTimeoutEx

--- packages/timer/src/index.ts
+++ packages/timer/src/index.ts
@@ -1,6 +1,7 @@
 interface TimeoutInfo {
   delay: number
   func: () => void
+  id: string
 }
 
 let timeouts: TimeoutInfo[] = []
@@ -14,12 +15,18 @@ const timerFunc = () => {
     timeout.func()
   }
   const notFiring = timeouts.filter((timeout) => timeout.delay > 100)
-  timeouts = notFiring.map((timeout) => ({ delay: timeout.delay - 100, func: timeout.func }))
+  timeouts = notFiring.map((timeout) => ({ delay: timeout.delay - 100, func: timeout.func, id: timeout.id }))
 }
 
 export const setTimeoutEx = (func: () => void, delay: number) => {
   if (timeouts.length === 0) {
     setTimeout(timerFunc, 100)
   }
-  timeouts.push({ delay, func })
+  const id = `${Date.now()}|${Math.random() * 9_999_999_999}`
+  timeouts.push({ delay, func, id })
+  return id
+}
+
+export const clearTimeoutEx = (id: string) => {
+  timeouts = timeouts.filter((timeout) => timeout.id !== id)
 }

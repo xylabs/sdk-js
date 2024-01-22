d3ca7b2498ed3f9ff79537d826ca19806613e91e
Arie Trouw <arie.trouw@xylabs.com>
2024-01-22 12:20:40 -0800

    retry types

--- packages/retry/src/retry.ts
+++ packages/retry/src/retry.ts
@@ -3,12 +3,15 @@ import { Promisable } from '@xylabs/promise'
 
 export interface RetryConfig<T = unknown> {
   backoff?: number
-  complete?: (result?: T) => boolean
   interval?: number
   retries?: number
 }
 
-export const retry = async <T = unknown>(func: () => Promisable<T | undefined>, config?: RetryConfig<T>): Promise<T | undefined> => {
+export interface RetryConfigWithComplete<T = unknown> extends RetryConfig<T> {
+  complete?: (result?: T) => boolean
+}
+
+export const retry = async <T = unknown>(func: () => Promisable<T | undefined>, config?: RetryConfigWithComplete<T>): Promise<T | undefined> => {
   const { complete = (value: T | undefined) => value !== undefined, retries = 0, interval = 100, backoff = 2 } = config ?? {}
   const result = await func()
   if (complete(result)) {

7fe3b1a8326134e780101ef2d0a88408c72d0361
Arie Trouw <arie.trouw@xylabs.com>
2024-01-26 12:31:26 -0800

    disallowing naked string in assertEx message - 2.14.0

--- packages/assert/src/index.ts
+++ packages/assert/src/index.ts
@@ -10,10 +10,15 @@ export type AssertExErrorFunc<T, R extends Error> = (value?: T | null) => R
  * @throws AssertExError
  * @returns Value of expression
  */
-function assertEx<T>(expr?: T | null, message?: string): T
-function assertEx<T>(expr?: T | null, messageFunc?: AssertExMessageFunc<T>): T
-function assertEx<T, R extends Error>(expr?: T | null, errorFunc?: AssertExErrorFunc<T, R>): T
-function assertEx<T, R extends Error, P extends string | AssertExMessageFunc<T> | AssertExErrorFunc<T, R>>(expr?: T | null, messageOrFunc?: P): T {
+function assertEx<T>(expr: T | null | undefined): T
+/** deprecated - replace string with () => string */
+function assertEx<T>(expr: T | null | undefined, message?: string): T
+function assertEx<T>(expr: T | null | undefined, messageFunc?: AssertExMessageFunc<T>): T
+function assertEx<T, R extends Error>(expr: T | null | undefined, errorFunc?: AssertExErrorFunc<T, R>): T
+function assertEx<T, R extends Error, P extends string | AssertExMessageFunc<T> | AssertExErrorFunc<T, R>>(
+  expr: T | null | undefined,
+  messageOrFunc?: P,
+): T {
   if (expr) return expr
   if (typeof messageOrFunc === 'function') {
     const errorOrMessage = messageOrFunc(expr)

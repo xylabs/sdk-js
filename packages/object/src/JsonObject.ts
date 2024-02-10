df2bf24c90937d343e3d9fe46b727469d3452d34
Arie Trouw <arie.trouw@xylabs.com>
2024-02-10 13:17:46 -0800

    is functions

--- packages/object/src/JsonObject.ts
+++ packages/object/src/JsonObject.ts
@@ -1,3 +1,34 @@
+import { isObject } from './isObject'
+
 export type JsonValue = string | number | boolean | null | JsonObject | JsonArray
 export type JsonObject = { [key: string]: JsonValue }
 export type JsonArray = JsonValue[]
+
+export const isJsonValue = (value: unknown): value is JsonValue => {
+  switch (typeof value) {
+    case 'string':
+    case 'number':
+    case 'boolean': {
+      return true
+    }
+    default: {
+      return value === null || isJsonObject(value) || isJsonArray(value)
+    }
+  }
+}
+
+export const isJsonArray = (value: unknown): value is JsonArray => {
+  return Array.isArray(value) && !value.some((item) => !isJsonValue(item))
+}
+
+export const isValidJsonFieldPair = ([key, value]: [key: unknown, value: unknown]) => {
+  return typeof key === 'string' && isJsonValue(value)
+}
+
+export const isJsonObject = (value: unknown): value is JsonObject => {
+  return (
+    isObject(value) &&
+    //check if all keys are strings
+    !Object.entries(value).some((item) => !isValidJsonFieldPair(item))
+  )
+}

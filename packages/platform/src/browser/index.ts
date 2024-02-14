3e84b62aea35545a4cf384845033c3aa552b4de3
Arie Trouw <arie.trouw@xylabs.com>
2024-02-14 12:47:05 -0800

    Fixed isBrowser to not crash in WebWorker

--- packages/platform/src/browser/index.ts
+++ packages/platform/src/browser/index.ts
@@ -1,13 +1,13 @@
 import type { subtle as subtleType } from 'node:crypto'
 
 export const isBrowser = () => {
-  return window?.document !== undefined
+  return !isWebworker() && window && window.document !== undefined
 }
 
 export const isWebworker = () => {
   return typeof self === 'object' && self.constructor?.name === 'DedicatedWorkerGlobalScope'
 }
 
-export const getGlobal = <T extends object>() => (isBrowser() ? (window as unknown as T) : isWebworker() ? (self as unknown as T) : globalThis)
+export const getGlobal = <T extends object>() => (isWebworker() ? (self as unknown as T) : isBrowser() ? (window as unknown as T) : globalThis)
 
 export const subtle = getGlobal<Window>().crypto.subtle as typeof subtleType

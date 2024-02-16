e94cecd79bcc28f7fe6aa74543a601a0105bcc00
Arie Trouw <arie.trouw@xylabs.com>
2024-02-16 13:16:10 -0800

    toUint8Array slight speed improvement

--- packages/arraybuffer/src/toUint8Array.ts
+++ packages/arraybuffer/src/toUint8Array.ts
@@ -4,6 +4,6 @@ export function toUint8Array(value: undefined, padLength?: number, base?: number
 export function toUint8Array(value: ArrayBuffer | string, padLength?: number, base?: number): Uint8Array
 export function toUint8Array(value: ArrayBuffer | string | undefined, padLength?: number, base?: number): Uint8Array | undefined
 export function toUint8Array(value?: ArrayBuffer | string, padLength?: number, base?: number): Uint8Array | undefined {
-  const arrayBuffer = toArrayBuffer(value, padLength, base)
+  const arrayBuffer = typeof value === 'string' ? toArrayBuffer(value, padLength, base) : value
   return arrayBuffer ? new Uint8Array(arrayBuffer) : undefined
 }

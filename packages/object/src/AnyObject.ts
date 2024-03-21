75c9c4dc553d76d83f3ffd892895bf6147ef85ad
Arie Trouw <arie.trouw@xylabs.com>
2024-03-20 17:45:40 -0700

    v3.0.21

--- packages/object/src/AnyObject.ts
+++ packages/object/src/AnyObject.ts
@@ -1,8 +1,7 @@
-import { TypedKey, TypedObject } from '@xylabs/promise'
+import { TypedKey } from '@xylabs/promise'
 
-import { EmptyObject } from './EmptyObject'
 /**
  * Any object, which means that it does not enforce the set of fields that it has.  Extending from AnyObject
  * will result in a type that includes the universal set of field names
  */
-export type AnyObject = TypedObject | EmptyObject | Record<TypedKey, unknown>
+export type AnyObject = Record<TypedKey, unknown>

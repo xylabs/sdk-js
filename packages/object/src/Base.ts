3f5af5775381dae5753bb2cfb919ec05d22386ea
Arie Trouw <arie.trouw@xylabs.com>
2024-04-06 21:57:54 -0700

    v3.1.5

--- packages/object/src/Base.ts
+++ packages/object/src/Base.ts
@@ -14,18 +14,18 @@ export abstract class Base<TParams extends BaseParams | undefined = BaseParams> 
   static defaultLogger?: Logger
   static readonly uniqueName = globallyUnique(this.name, this, 'xyo')
 
-  private _params?: TParams
+  private _params: TParams
 
-  constructor(params?: TParams) {
+  constructor(params: TParams) {
     this._params = params
     params?.logger?.debug(`Base constructed [${Object(this).name}]`)
   }
 
-  get params() {
-    return this._params
+  get logger() {
+    return this.params?.logger ?? Base.defaultLogger
   }
 
-  protected get logger() {
-    return this.params?.logger ?? Base.defaultLogger
+  get params() {
+    return this._params
   }
 }

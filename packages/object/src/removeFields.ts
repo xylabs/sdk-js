d9f5a86f59f784f5d30136b7801b35a4be9cf2d0
Arie Trouw <arie.trouw@xylabs.com>
2024-04-08 16:50:21 -0700

    removeFields

---
+++ packages/object/src/removeFields.ts
@@ -0,0 +1,9 @@
+import { EmptyObject } from './EmptyObject'
+
+export const removeFields = <T extends EmptyObject, K extends keyof T>(obj: T, fields: K[]): Omit<T, K> => {
+  const clone = { ...obj }
+  for (const field of fields) {
+    delete clone[field]
+  }
+  return clone
+}

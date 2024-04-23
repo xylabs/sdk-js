fd18a0c0d32a96a063eac9b9e4d818951a9977b3
Arie Trouw <arie.trouw@xylabs.com>
2024-04-23 08:20:24 -0700

    deploy 3.1.11

--- packages/object/src/globallyUnique.ts
+++ packages/object/src/globallyUnique.ts
@@ -1,20 +1,31 @@
 interface XylabsGlobalThis {
   xylabs: {
     unique: Record<string, unknown>
+    uniqueDisabled?: boolean
   }
+}
+
+const xyoGlobal = () => {
+  return ((globalThis as unknown as XylabsGlobalThis).xylabs = (globalThis as unknown as XylabsGlobalThis).xylabs ?? {})
+}
+
+export const disableGloballyUnique = () => {
+  xyoGlobal().uniqueDisabled = true
 }
 
 export const globallyUnique = (name: string, value: unknown, domain = 'global') => {
   const uniqueName = [domain, name].join(':')
-  const xyo = ((globalThis as unknown as XylabsGlobalThis).xylabs = (globalThis as unknown as XylabsGlobalThis).xylabs ?? {})
-  const unique = (xyo.unique = xyo.unique ?? {})
-  if (unique[uniqueName] === undefined) {
-    unique[uniqueName] = value
-  } else {
-    if (unique[uniqueName] !== value) {
-      throw new Error(
-        `Global unique item ${uniqueName} already defined.  Make sure you are not importing two versions of the package that contains this item`,
-      )
+  if (!xyoGlobal().uniqueDisabled) {
+    const xyo = ((globalThis as unknown as XylabsGlobalThis).xylabs = (globalThis as unknown as XylabsGlobalThis).xylabs ?? {})
+    const unique = (xyo.unique = xyo.unique ?? {})
+    if (unique[uniqueName] === undefined) {
+      unique[uniqueName] = value
+    } else {
+      if (unique[uniqueName] !== value) {
+        throw new Error(
+          `Global unique item ${uniqueName} already defined.  Make sure you are not importing two versions of the package that contains this item`,
+        )
+      }
     }
   }
   return uniqueName

7833dff1be466e0b91543c82a5d0e8901ebe9c2f
Arie Trouw <arie.trouw@xylabs.com>
2024-07-16 14:11:53 -0700

    jest tests fixed

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,16 @@ const generateJestConfig = ({ esModules }: { esModules: string[] }) => {
   const esModulesList = Array.isArray(esModules) ? esModules.join('|') : esModules
   return {
     coveragePathIgnorePatterns: ['<rootDir>/(.*)/dist'],
+    extensionsToTreatAsEsm: ['.ts'],
+    globalSetup: './jestSetup/globalSetup.ts',
+    globalTeardown: './jestSetup/globalTeardown.ts',
     moduleNameMapper: {
       '^(\\.{1,2}/.*)\\.js$': '$1',
     },
-    preset: 'ts-jest/presets/default-esm',
+    preset: 'ts-jest',
     setupFiles: ['dotenv/config'],
-    setupFilesAfterEnv: ['jest-sorted', 'jest-extended/all'],
+    setupFilesAfterEnv: ['jest-sorted', 'jest-extended/all', './jestSetup/setupFiles.ts'],
+    testEnvironment: 'node',
     testRegex: String.raw`(/__tests__/.*|(\.|/)((!perf\.)test|spec))\.tsx?$`,
     testTimeout: 200_000,
     transform: {
@@ -16,6 +20,7 @@ const generateJestConfig = ({ esModules }: { esModules: string[] }) => {
         'ts-jest',
         {
           tsconfig: 'tsconfig.test.json',
+          useESM: true,
         },
       ],
     },
@@ -23,7 +28,7 @@ const generateJestConfig = ({ esModules }: { esModules: string[] }) => {
   }
 }
 
-const config = generateJestConfig({ esModules: ['is-ip', 'ip-regex', 'lodash-es', 'uuid', 'lodash-es'] })
+const config = generateJestConfig({ esModules: ['is-ip', 'ip-regex', 'lodash-es', 'uuid', 'lodash-es', 'quick-lru'] })
 
 // eslint-disable-next-line import/no-default-export
 export default config

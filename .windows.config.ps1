bdabc8921dc72154b2cc45df60ca7278d32af6e3
Arie Trouw <arie.trouw@xylabs.com>
2024-07-17 14:56:27 -0700

    reverted windows fixes that did not work

--- .windows.config.ps1
+++
@@ -1,9 +0,0 @@
-# Check if .yarnrc.windows.yml exists
-if (Test-Path -Path ".\.yarnrc.windows.yml") {
-    # Copy .yarnrc.windows.yml to .yarnrc.yml
-    Copy-Item -Path ".\.yarnrc.windows.yml" -Destination ".\yarnrc.yml" -Force
-    Write-Output "Copied .yarnrc.windows.yml to .yarnrc.yml successfully."
-} else {
-    Write-Output "Error: .yarnrc.windows.yml file does not exist."
-    exit 1
-}

2f6f8355075d8a255b6b314e619d83f046d3acb0
Arie Trouw <arie.trouw@xylabs.com>
2024-11-25 11:19:08 -0800

    Deploy
    4.4.2 - toUint8Array params fixed

--- packages/arraybuffer/src/toUint8Array.ts
+++ packages/arraybuffer/src/toUint8Array.ts
@@ -1,9 +1,9 @@
 import { toArrayBuffer } from './toArrayBuffer.ts'
 
 export function toUint8Array(value: undefined, padLength?: number, base?: number): undefined
-export function toUint8Array(value: ArrayBuffer | string, padLength?: number, base?: number): Uint8Array
-export function toUint8Array(value: ArrayBuffer | string | undefined, padLength?: number, base?: number): Uint8Array | undefined
-export function toUint8Array(value?: ArrayBuffer | string, padLength?: number, base?: number): Uint8Array | undefined {
+export function toUint8Array(value: ArrayBufferLike | string, padLength?: number, base?: number): Uint8Array
+export function toUint8Array(value: ArrayBufferLike | string | undefined, padLength?: number, base?: number): Uint8Array | undefined
+export function toUint8Array(value?: ArrayBufferLike | string, padLength?: number, base?: number): Uint8Array | undefined {
   const arrayBuffer = typeof value === 'string' ? toArrayBuffer(value, padLength, base) : value
   return arrayBuffer ? new Uint8Array(arrayBuffer) : undefined
 }

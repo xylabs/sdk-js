c6a023c8d6652ee69110990744c8e26cd5d70678
Arie Trouw <arie.trouw@xylabs.com>
2024-12-26 15:32:40 -0800

    bigint support
    toArrayBuffer and toUInt8Array

--- packages/arraybuffer/src/toArrayBuffer.ts
+++ packages/arraybuffer/src/toArrayBuffer.ts
@@ -16,13 +16,19 @@ const stringToUInt8Array = (value: string, base = 16): Uint8Array => {
 }
 
 export function toArrayBuffer(value: undefined, padLength?: number, base?: number): undefined
-export function toArrayBuffer(value: ArrayBufferLike | string, padLength?: number, base?: number): ArrayBufferLike
-export function toArrayBuffer(value: ArrayBufferLike | string | undefined, padLength?: number, base?: number): ArrayBufferLike | undefined
-export function toArrayBuffer(value?: ArrayBufferLike | string, padLength?: number, base?: number): ArrayBufferLike | undefined {
+export function toArrayBuffer(value: ArrayBufferLike | bigint | string, padLength?: number, base?: number): ArrayBufferLike
+export function toArrayBuffer(value: ArrayBufferLike | bigint | string | undefined, padLength?: number, base?: number): ArrayBufferLike | undefined
+export function toArrayBuffer(value?: ArrayBufferLike | bigint | string, padLength?: number, base?: number): ArrayBufferLike | undefined {
   if (value === undefined) return undefined
 
+  if (typeof value === 'bigint' && value < 0) {
+    throw new Error(`negative bigint values are not supported: ${typeof value}`)
+  }
+
   let result: Uint8Array | undefined
-    = typeof value === 'string' ? stringToUInt8Array(value, base) : (new Uint8Array(value))
+    = typeof value === 'string'
+      ? stringToUInt8Array(value, base)
+      : typeof value === 'bigint' ? stringToUInt8Array(value.toString(16)) : (new Uint8Array(value))
 
   if (result === undefined) {
     throw new Error(`toArrayBuffer - Unknown type: ${typeof value}`)
--- packages/arraybuffer/src/toUint8Array.ts
+++ packages/arraybuffer/src/toUint8Array.ts
@@ -1,9 +1,9 @@
 import { toArrayBuffer } from './toArrayBuffer.ts'
 
 export function toUint8Array(value: undefined, padLength?: number, base?: number): undefined
-export function toUint8Array(value: ArrayBufferLike | string, padLength?: number, base?: number): Uint8Array
-export function toUint8Array(value: ArrayBufferLike | string | undefined, padLength?: number, base?: number): Uint8Array | undefined
-export function toUint8Array(value?: ArrayBufferLike | string, padLength?: number, base?: number): Uint8Array | undefined {
-  const arrayBuffer = typeof value === 'string' ? toArrayBuffer(value, padLength, base) : value
+export function toUint8Array(value: ArrayBufferLike | bigint | string, padLength?: number, base?: number): Uint8Array
+export function toUint8Array(value: ArrayBufferLike | bigint | string | undefined, padLength?: number, base?: number): Uint8Array | undefined
+export function toUint8Array(value?: ArrayBufferLike | bigint | string, padLength?: number, base?: number): Uint8Array | undefined {
+  const arrayBuffer = toArrayBuffer(value, padLength, base)
   return arrayBuffer ? new Uint8Array(arrayBuffer) : undefined
 }

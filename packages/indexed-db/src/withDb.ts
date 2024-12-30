90799c96b71377e4f705da140111d0954e352c65
Arie Trouw <arie.trouw@xylabs.com>
2024-12-30 11:58:18 -0800

    withDB R type

--- packages/indexed-db/src/withDb.ts
+++ packages/indexed-db/src/withDb.ts
@@ -4,7 +4,10 @@ import { openDB } from 'idb'
 
 import type { ObjectStore } from './ObjectStore.ts'
 
-export async function withDb<T extends EmptyObject = EmptyObject>(dbName: string, callback: (db: IDBPDatabase<ObjectStore<T>>) => Promise<T> | T): Promise<T> {
+export async function withDb<T extends EmptyObject = EmptyObject, R = T>(
+  dbName: string,
+  callback: (db: IDBPDatabase<ObjectStore<T>>) => Promise<R> | R,
+): Promise<R> {
   const db = await openDB<ObjectStore<T>>(dbName)
   try {
     return await callback(db)

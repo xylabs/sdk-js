e2da936d60549139175adf9c75a7afd4cc147c33
Arie Trouw <arie.trouw@xylabs.com>
2024-12-31 13:31:27 -0800

    withDb types
    4.4.33

--- packages/indexed-db/src/withDb.ts
+++ packages/indexed-db/src/withDb.ts
@@ -2,11 +2,11 @@ import type { EmptyObject } from '@xylabs/object'
 import type { DBSchema, IDBPDatabase } from 'idb'
 import { openDB } from 'idb'
 
-export async function withDb<T extends DBSchema = DBSchema, R = EmptyObject>(
+export async function withDb<DBTypes extends DBSchema | unknown = unknown, R = EmptyObject>(
   dbName: string,
-  callback: (db: IDBPDatabase<T>) => Promise<R> | R,
+  callback: (db: IDBPDatabase<DBTypes>) => Promise<R> | R,
 ): Promise<R> {
-  const db = await openDB<T>(dbName)
+  const db = await openDB<DBTypes>(dbName)
   try {
     return await callback(db)
   } finally {

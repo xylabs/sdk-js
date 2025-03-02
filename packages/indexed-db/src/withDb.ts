0485b162ac5c05e7907854d76878b4d712e45696
Arie Trouw <arie.trouw@xylabs.com>
2025-03-02 14:00:20 -0800

    withDB Mutex

--- packages/indexed-db/src/withDb.ts
+++ packages/indexed-db/src/withDb.ts
@@ -1,5 +1,6 @@
 import type { Logger } from '@xylabs/logger'
 import type { EmptyObject } from '@xylabs/object'
+import { Mutex } from 'async-mutex'
 import type {
   DBSchema, IDBPDatabase, StoreNames,
 } from 'idb'
@@ -9,24 +10,28 @@ import { checkDbNeedsUpgrade } from './checkDbNeedsUpgrade.ts'
 import { createStoreDuringUpgrade } from './createStoreDuringUpgrade.ts'
 import { buildStandardIndexName, type IndexDescription } from './IndexDescription.ts'
 
+const dbMutexes: Record<string, Mutex> = {}
+
 export async function withDb<DBTypes extends DBSchema | unknown = unknown, R = EmptyObject>(
   dbName: string,
   callback: (db: IDBPDatabase<DBTypes>) => Promise<R> | R,
   expectedIndexes?: Record<string, IndexDescription[]>,
   logger?: Logger,
 ): Promise<R> {
-  const versionToOpen = expectedIndexes === undefined ? undefined : await checkDbNeedsUpgrade(dbName, expectedIndexes, logger)
-  const db = await openDB<DBTypes>(dbName, versionToOpen, {
-    blocked(currentVersion, blockedVersion, event) {
-      logger?.warn(`IndexedDb: Blocked from upgrading from ${currentVersion} to ${blockedVersion}`, event)
-    },
-    blocking(currentVersion, blockedVersion, event) {
-      logger?.warn(`IndexedDb: Blocking upgrade from ${currentVersion} to ${blockedVersion}`, event)
-    },
-    terminated() {
-      logger?.log('IndexedDb: Terminated')
-    },
-    upgrade(db, oldVersion, newVersion, transaction) {
+  dbMutexes[dbName] = dbMutexes[dbName] ?? new Mutex()
+  const db = await dbMutexes[dbName].runExclusive(async () => {
+    const versionToOpen = expectedIndexes === undefined ? undefined : await checkDbNeedsUpgrade(dbName, expectedIndexes, logger)
+    const db = await openDB<DBTypes>(dbName, versionToOpen, {
+      blocked(currentVersion, blockedVersion, event) {
+        logger?.warn(`IndexedDb: Blocked from upgrading from ${currentVersion} to ${blockedVersion}`, event)
+      },
+      blocking(currentVersion, blockedVersion, event) {
+        logger?.warn(`IndexedDb: Blocking upgrade from ${currentVersion} to ${blockedVersion}`, event)
+      },
+      terminated() {
+        logger?.log('IndexedDb: Terminated')
+      },
+      upgrade(db, oldVersion, newVersion, transaction) {
       /* if (oldVersion !== newVersion) {
         logger?.log(`IndexedDb: Upgrading from ${oldVersion} to ${newVersion}`)
         const objectStores = transaction.objectStoreNames
@@ -38,20 +43,22 @@ export async function withDb<DBTypes extends DBSchema | unknown = unknown, R = E
           }
         }
       } */
-      if (expectedIndexes) {
-        for (const [storeName, indexes] of Object.entries(expectedIndexes)) {
-          if (db.objectStoreNames.contains(storeName as StoreNames<DBTypes>)) {
-            continue
-          }
-          const indexesToCreate = indexes.map(idx => ({
-            ...idx,
-            name: buildStandardIndexName(idx),
+        if (expectedIndexes) {
+          for (const [storeName, indexes] of Object.entries(expectedIndexes)) {
+            if (db.objectStoreNames.contains(storeName as StoreNames<DBTypes>)) {
+              continue
+            }
+            const indexesToCreate = indexes.map(idx => ({
+              ...idx,
+              name: buildStandardIndexName(idx),
             // eslint-disable-next-line unicorn/no-array-reduce
-          })).reduce((acc, idx) => acc.set(idx.name, idx), new Map<string, IndexDescription>()).values()
-          createStoreDuringUpgrade(db, storeName as StoreNames<DBTypes>, [...indexesToCreate], logger)
+            })).reduce((acc, idx) => acc.set(idx.name, idx), new Map<string, IndexDescription>()).values()
+            createStoreDuringUpgrade(db, storeName as StoreNames<DBTypes>, [...indexesToCreate], logger)
+          }
         }
-      }
-    },
+      },
+    })
+    return db
   })
   try {
     return await callback(db)

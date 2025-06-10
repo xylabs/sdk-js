4ff5b3e3da74500990f79fa34ab2a9d754c23088
Arie Trouw <arie.trouw@xylabs.com>
2025-06-10 11:45:11 -0700

    upyarn

--- knip.config.ts
+++ knip.config.ts
@@ -2,35 +2,31 @@ import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
   ignoreDependencies: ['@xylabs/ts-scripts-yarn3'],
-  entry: ['src/index.ts', 'xy.config.ts'],
   workspaces: {
     '.': {
-      entry: ['src/index.ts', 'eslint.config.mjs', 'xy.config.ts'],
+      entry: ['src/index.ts'],
       ignoreDependencies: [
-        'eslint',
-        '@typescript-eslint/eslint-plugin',
         'eslint-import-resolver-typescript',
       ],
     },
     'packages/buffer': {
       ignoreDependencies: ['buffer'],
-      entry: ['src/index.ts', 'src/node/index.ts', 'src/browser/index.ts'],
+      entry: ['src/node/index.ts', 'src/browser/index.ts'],
     },
-    'packages/*': { entry: ['src/index.ts', 'src/node/index.ts', 'src/browser/index.ts', 'src/neutral/index.ts'] },
+    'packages/*': { entry: ['src/index.ts'] },
+    'packages/platform': { entry: ['src/node/index.ts', 'src/browser/index.ts'] },
+    'packages/crypto': { entry: ['src/node/index.ts', 'src/browser/index.ts'] },
+    'packages/forget': { entry: ['src/index.ts', 'src/index-node.ts'] },
+    'packages/url': { entry: ['src/node/index.ts', 'src/browser/index.ts', 'src/neutral/index.ts'] },
     'packages/threads': {
       ignoreDependencies: ['is-observable-2-1-0'],
       entry: [
-        'src/index.ts',
+        'src/index-node.ts',
+        'src/index-browser.ts',
         'src/master/implementation.browser.ts',
         'src/master/implementation.node.ts',
         'index.mjs',
-        'observable.{mjs,js}',
-        'register.{mjs,js}',
-        'worker.{mjs,js}',
         'src/master/register.ts',
-        'src/worker/bundle-entry.ts',
-        'test-tooling/**/*.{ts,js}',
-        'test/**/*.{ts,js}',
       ],
     },
     'packages/static-implements': { ignoreDependencies: ['tslib'] },

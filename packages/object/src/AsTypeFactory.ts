298d71bb504f53f1e299991effabd752ecb7ed56
Arie Trouw <arie.trouw@xylabs.com>
2025-06-18 12:08:54 -0700

    required config options for AsTypeFactory

--- packages/object/src/AsTypeFactory.ts
+++ packages/object/src/AsTypeFactory.ts
@@ -3,10 +3,18 @@ import { assertDefinedEx } from '@xylabs/assert'
 import type { Logger } from '@xylabs/logger'
 import type { AnyNonPromise } from '@xylabs/promise'
 import { isPromise } from '@xylabs/promise'
-import type { TypedValue } from '@xylabs/typeof'
+import { isTruthy, type TypedValue } from '@xylabs/typeof'
 
 export interface TypeCheckConfig {
   log?: boolean | Logger
+}
+
+export interface TypeCheckRequiredConfig extends TypeCheckConfig {
+  required: true
+}
+
+export interface TypeCheckOptionalConfig extends TypeCheckConfig {
+  required: false
 }
 
 export type StringOrAlertFunction<T extends AnyNonPromise> = string | AssertExMessageFunc<T>
@@ -19,9 +27,11 @@ export type AsOptionalTypeFunction<T extends AnyNonPromise = AnyNonPromise> = {
 
 export type AsTypeFunction<T extends AnyNonPromise = AnyNonPromise> = {
   <TType extends T>(value: AnyNonPromise): TType | undefined
-  <TType extends T>(value: AnyNonPromise, config: TypeCheckConfig): TType | undefined
+  <TType extends T>(value: AnyNonPromise, config: TypeCheckRequiredConfig): TType
+  <TType extends T>(value: AnyNonPromise, config: TypeCheckConfig | TypeCheckOptionalConfig): TType | undefined
   <TType extends T>(value: AnyNonPromise, assert: StringOrAlertFunction<TType>): TType | undefined
-  <TType extends T>(value: AnyNonPromise, assert: StringOrAlertFunction<TType>, config: TypeCheckConfig): TType | undefined
+  <TType extends T>(value: AnyNonPromise, assert: StringOrAlertFunction<TType>, config: TypeCheckRequiredConfig): TType
+  <TType extends T>(value: AnyNonPromise, assert: StringOrAlertFunction<TType>, config: TypeCheckConfig | TypeCheckOptionalConfig): TType | undefined
 }
 
 export const AsTypeFactory = {
@@ -31,20 +41,22 @@ export const AsTypeFactory = {
       assertOrConfig?: StringOrAlertFunction<T> | TypeCheckConfig,
       config?: TypeCheckConfig,
     ): T | undefined => {
-      if (value === undefined || value === null) {
-        return undefined
-      }
-
-      if (isPromise(value)) {
-        throw new TypeError('un-awaited promises may not be sent to "as" functions')
-      }
-
       // when used as a predicate, it seems that the index is passed as the second parameter (filter,map)
       const isPredicate = typeof assertOrConfig === 'number'
       const resolvedAssert = isPredicate
         ? undefined
         : (typeof assertOrConfig === 'object' ? undefined : assertOrConfig) as (StringOrAlertFunction<T> | undefined)
       const resolvedConfig = isPredicate ? undefined : typeof assertOrConfig === 'object' ? assertOrConfig : config
+
+      // only return undefined if not required
+      const required = isTruthy((resolvedConfig as (TypeCheckRequiredConfig | undefined))?.required)
+      if (!required && (value === undefined || value === null)) {
+        return undefined
+      }
+
+      if (isPromise(value)) {
+        throw new TypeError('un-awaited promises may not be sent to "as" functions')
+      }
 
       const result = typeCheck(value, resolvedConfig) ? (value as T) : undefined
 

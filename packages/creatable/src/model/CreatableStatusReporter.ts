9a9fd011500e95c57609779f09da19facc6b998f
Arie Trouw <arie.trouw@xylabs.com>
2025-06-30 13:19:10 -0700

    Deploy
    4.12.28

--- packages/creatable/src/model/CreatableStatusReporter.ts
+++ packages/creatable/src/model/CreatableStatusReporter.ts
@@ -2,7 +2,7 @@ import type { BaseClassName } from '@xylabs/base'
 
 export type CreatableStatus = 'creating' | 'created' | 'starting' | 'started' | 'stopping' | 'stopped' | 'error'
 
-export interface CreatableStatusReporter<T extends CreatableStatus = CreatableStatus> {
-  report(name: BaseClassName, status: Exclude<T, 'error'>, progress?: number): void
-  report(name: BaseClassName, status: Extract<T, 'error'>, error?: Error): void
+export interface CreatableStatusReporter<T extends void | string = void> {
+  report(name: BaseClassName, status: Exclude<T extends void ? CreatableStatus : CreatableStatus | T, 'error'>, progress?: number): void
+  report(name: BaseClassName, status: Extract<T extends void ? CreatableStatus : CreatableStatus | T, 'error'>, error?: Error): void
 }

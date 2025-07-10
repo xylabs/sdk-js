7a0db349ef8a82e763eeb5b0ca8044a03c4eff50
Arie Trouw <arie.trouw@xylabs.com>
2025-07-10 16:53:10 -0700

    CreatableInstance implements EventEmitter

--- packages/creatable/src/model/CreatableInstance.ts
+++ packages/creatable/src/model/CreatableInstance.ts
@@ -1,8 +1,9 @@
-import type { EventData } from '@xylabs/events'
+import type { EventData, EventEmitter } from '@xylabs/events'
 
 import type { CreatableName, CreatableParams } from './CreatableParams.ts'
 
-export interface CreatableInstance<TParams extends CreatableParams = CreatableParams, TEventData extends EventData = EventData> {
+export interface CreatableInstance<TParams extends CreatableParams = CreatableParams,
+  TEventData extends EventData = EventData> extends EventEmitter<TEventData> {
   eventData: TEventData
   name: CreatableName
   params: TParams

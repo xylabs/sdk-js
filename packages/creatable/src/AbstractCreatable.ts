cf51185b37b8611dddbc97e1af7b150af2060e74
Arie Trouw <arie.trouw@xylabs.com>
2025-10-28 11:42:16 -0700

    Creatable status management updates

--- packages/creatable/src/AbstractCreatable.ts
+++ packages/creatable/src/AbstractCreatable.ts
@@ -3,7 +3,8 @@ import type { EventData } from '@xylabs/events'
 import { BaseEmitter } from '@xylabs/events'
 import { type Logger } from '@xylabs/logger'
 import type { Promisable } from '@xylabs/promise'
-import { isError } from '@xylabs/typeof'
+import { isError, isNumber } from '@xylabs/typeof'
+import { Mutex } from 'async-mutex'
 
 import {
   type Creatable, creatable, CreatableFactory,
@@ -12,6 +13,7 @@ import { Factory } from './Factory.ts'
 import { getFunctionName, getRootFunction } from './lib/index.ts'
 import type {
   CreatableInstance, CreatableName, CreatableParams,
+  CreatableStatus,
   Labels,
 } from './model/index.ts'
 
@@ -22,6 +24,8 @@ export class AbstractCreatable<TParams extends CreatableParams = CreatableParams
   TEventData extends EventData = EventData> extends BaseEmitter<Partial<TParams>, TEventData> {
   defaultLogger?: Logger
 
+  private _status: CreatableStatus | null = null
+  private _statusMutex = new Mutex()
   private _validatedParams?: TParams
 
   constructor(key: unknown, params: Partial<TParams>) {
@@ -38,6 +42,14 @@ export class AbstractCreatable<TParams extends CreatableParams = CreatableParams
     return this._validatedParams
   }
 
+  get startable() {
+    return this.status === 'created' || this.status === 'stopped'
+  }
+
+  get status(): CreatableStatus | null {
+    return this._status
+  }
+
   get statusReporter() {
     return this.params.statusReporter
   }
@@ -48,12 +60,12 @@ export class AbstractCreatable<TParams extends CreatableParams = CreatableParams
   ): Promise<T> {
     const params = await this.paramsHandler(inParams)
     const name: CreatableName = params.name ?? this.name as CreatableName
-    params.statusReporter?.report(name, 'creating')
     try {
       const instance = new this(AbstractCreatableConstructorKey, params)
+      instance.setStatus('creating')
       const initializedInstance = await this.createHandler(instance)
       await instance.createHandler()
-      params.statusReporter?.report(name, 'created')
+      instance.setStatus('created')
       return initializedInstance
     } catch (ex) {
       params.statusReporter?.report(name, 'error', isError(ex) ? ex : new Error(`Error creating: ${name}`))
@@ -75,7 +87,9 @@ export class AbstractCreatable<TParams extends CreatableParams = CreatableParams
     return { ...params }
   }
 
-  createHandler(): Promisable<void> {}
+  createHandler(): Promisable<void> {
+    assertEx(this._status === 'creating', () => `createHandler can not be called [status = ${this.status}]`)
+  }
 
   paramsValidator(params: Partial<TParams>): TParams {
     return { ...params, name: params.name ?? this.constructor.name } as TParams
@@ -83,28 +97,48 @@ export class AbstractCreatable<TParams extends CreatableParams = CreatableParams
 
   async start(): Promise<boolean> {
     this._noOverride('start')
-    try {
-      this.statusReporter?.report(this.name, 'starting')
-      await this.startHandler()
-      this.statusReporter?.report(this.name, 'started')
+    if (this.status === 'started') {
       return true
-    } catch (ex) {
-      this.statusReporter?.report(this.name, 'error', isError(ex) ? ex : new Error(`Error stopping: ${ex}`))
-      return false
     }
+    return await this._statusMutex.runExclusive(async () => {
+      try {
+        // check again in case it changed
+        if (this.status === 'started') {
+          return true
+        }
+        assertEx(this.startable, () => `Creatable [${this.name}] is not startable in status [${this.status}]`)
+        this.setStatus('starting')
+        await this.startHandler()
+        this.setStatus('started')
+        return true
+      } catch (ex) {
+        this.setStatus('error', isError(ex) ? ex : new Error(`Error starting: ${ex}`))
+        return false
+      }
+    })
   }
 
   async stop(): Promise<boolean> {
     this._noOverride('stop')
-    try {
-      this.statusReporter?.report(this.name, 'stopping')
-      await this.stopHandler()
-      this.statusReporter?.report(this.name, 'stopped')
+    if (this.status === 'stopped') {
       return true
-    } catch (ex) {
-      this.statusReporter?.report(this.name, 'error', isError(ex) ? ex : new Error(`Error stopping: ${ex}`))
-      return false
     }
+    return await this._statusMutex.runExclusive(async () => {
+      try {
+        // check again in case it changed
+        if (this.status === 'stopped') {
+          return true
+        }
+        assertEx(this.status === 'started', () => `Creatable [${this.name}] is not stoppable in status [${this.status}]`)
+        this.setStatus('stopping')
+        await this.stopHandler()
+        this.setStatus('stopped')
+        return true
+      } catch (ex) {
+        this.setStatus('error', isError(ex) ? ex : new Error(`Error stopping: ${ex}`))
+        return false
+      }
+    })
   }
 
   protected _noOverride(functionName = getFunctionName(3)) {
@@ -113,6 +147,26 @@ export class AbstractCreatable<TParams extends CreatableParams = CreatableParams
 
     const rootFunc = getRootFunction(this, functionName)
     assertEx(thisFunc === rootFunc, () => `Override not allowed for [${functionName}] - override ${functionName}Handler instead`)
+  }
+
+  protected setStatus(value: Exclude<CreatableStatus, 'error'>, progress?: number): void
+  protected setStatus(value: Extract<CreatableStatus, 'error'>, error?: Error): void
+  protected setStatus(value: CreatableStatus, progressOrError?: number | Error): void {
+    this._status = value
+    if (value !== null) {
+      if (value === 'error') {
+        if (isError(progressOrError)) {
+          this.statusReporter?.report(this.name, value, progressOrError)
+          return
+        }
+      } else {
+        if (isNumber(progressOrError)) {
+          this.statusReporter?.report(this.name, value, progressOrError)
+          return
+        }
+      }
+      this.statusReporter?.report(this.name, value)
+    }
   }
 
   protected startHandler(): Promisable<void> {
--- packages/creatable/src/model/CreatableStatusReporter.ts
+++ packages/creatable/src/model/CreatableStatusReporter.ts
@@ -3,6 +3,7 @@ import type { BaseClassName } from '@xylabs/base'
 export type CreatableStatus = 'creating' | 'created' | 'starting' | 'started' | 'stopping' | 'stopped' | 'error'
 
 export interface CreatableStatusReporter<T extends void | string = void> {
-  report(name: BaseClassName, status: Exclude<T extends void ? CreatableStatus : CreatableStatus | T, 'error'>, progress?: number): void
-  report(name: BaseClassName, status: Extract<T extends void ? CreatableStatus : CreatableStatus | T, 'error'>, error?: Error): void
+  report(name: BaseClassName, status: Exclude<T extends void ? CreatableStatus : CreatableStatus | T, 'error'>, progress: number): void
+  report(name: BaseClassName, status: Extract<T extends void ? CreatableStatus : CreatableStatus | T, 'error'>, error: Error): void
+  report(name: BaseClassName, status: T extends void ? CreatableStatus : CreatableStatus | T): void
 }

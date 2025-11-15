16048d14ce3f2b5904f93ddf305edbb363e11af5
Arie Trouw <arie.trouw@xylabs.com>
2025-11-14 16:37:45 -0800

    axios export pnpm safe

--- packages/axios/src/index.ts
+++ packages/axios/src/index.ts
@@ -1,9 +1,8 @@
+import { Axios } from 'axios'
+
 import { AxiosJson } from './AxiosJson.ts'
-import { AxiosJsonUncompressed } from './AxiosJsonUncompressed.ts'
 
 export * from './AxiosJson.ts'
-export * from './AxiosJsonUncompressed.ts'
 export { gzip } from 'pako'
 
-export const axios = new AxiosJson()
-export const axiosUncompressed = new AxiosJsonUncompressed()
+export const axios = new Axios(AxiosJson.axiosConfig())

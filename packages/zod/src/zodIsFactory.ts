277f87aa708fd5fb3ca640d7f6c748bfda2d87df
Arie Trouw <arie.trouw@xylabs.com>
2025-11-20 18:28:22 -0800

    zodIsFactory fixed with T

--- packages/zod/src/zodIsFactory.ts
+++ packages/zod/src/zodIsFactory.ts
@@ -1,5 +1,5 @@
 import type z from 'zod'
 
-export function zodIsFactory<T>(zod: z.ZodType<T>) {
-  return (value: unknown): value is T => zod.safeParse(value).success
+export function zodIsFactory<TZod>(zod: z.ZodType<TZod>) {
+  return <T>(value: T): value is T & TZod => zod.safeParse(value).success
 }

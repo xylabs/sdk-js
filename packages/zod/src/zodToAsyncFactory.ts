938ef939cfb251f1894f5cf054cda421c6e29ef6
Arie Trouw <arie.trouw@xylabs.com>
2025-11-24 12:46:58 -0800

    to factory types

--- packages/zod/src/zodToAsyncFactory.ts
+++ packages/zod/src/zodToAsyncFactory.ts
@@ -1,7 +1,18 @@
+import { isDefined } from '@xylabs/typeof'
 import type z from 'zod'
 
+import type { ZodFactoryConfig } from './Config.ts'
 import { zodAsAsyncFactory } from './zodAsAsyncFactory.ts'
 
-export function zodToAsyncFactory<T>(zod: z.ZodType<T>, name: string) {
-  return zodAsAsyncFactory<T>(zod, name)
+export function zodToAsyncFactory<TZod>(zod: z.ZodType<TZod>, name: string) {
+  const as = zodAsAsyncFactory<TZod>(zod, name)
+  function toFunc<T>(value: T): Promise<(T & TZod) | undefined>
+  function toFunc<T>(value: T, assert: ZodFactoryConfig): Promise<(T & TZod)>
+  async function toFunc<T>(value: T, assert?: ZodFactoryConfig): Promise<(T & TZod) | undefined> {
+    if (isDefined(assert)) {
+      return await as(value, assert)
+    }
+    return await as(value)
+  }
+  return toFunc
 }
--- packages/zod/src/zodToFactory.ts
+++ packages/zod/src/zodToFactory.ts
@@ -1,7 +1,18 @@
+import { isDefined } from '@xylabs/typeof'
 import type z from 'zod'
 
+import type { ZodFactoryConfig } from './Config.ts'
 import { zodAsFactory } from './zodAsFactory.ts'
 
-export function zodToFactory<T>(zod: z.ZodType<T>, name: string) {
-  return zodAsFactory<T>(zod, name)
+export function zodToFactory<TZod>(zod: z.ZodType<TZod>, name: string) {
+  const as = zodAsFactory<TZod>(zod, name)
+  function toFunc<T>(value: T): (T & TZod) | undefined
+  function toFunc<T>(value: T, assert: ZodFactoryConfig): (T & TZod)
+  function toFunc<T>(value: T, assert?: ZodFactoryConfig): (T & TZod) | undefined {
+    if (isDefined(assert)) {
+      return as(value, assert)
+    }
+    return as(value)
+  }
+  return toFunc
 }

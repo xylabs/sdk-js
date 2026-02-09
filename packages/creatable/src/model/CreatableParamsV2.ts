7498da4e1bc6636a593d9b068d37f3e5f98a7875
Arie Trouw <arie.trouw@xylabs.com>
2026-02-09 15:50:32 -0800

    Deploy
    5.0.76

--- packages/creatable/src/model/CreatableParamsV2.ts
+++ packages/creatable/src/model/CreatableParamsV2.ts
@@ -1,10 +1,10 @@
-import { BaseContextZod } from '@xylabs/base'
+import { CachingContextZod } from '@xylabs/base'
 import { BaseEmitterParamsV2Zod } from '@xylabs/events'
 import z from 'zod'
 
 import { CreatableStatusReporterV2Zod } from './CreatableStatusReporter.zod.ts'
 
-export const CreatableContextZod = BaseContextZod.extend({ statusReporter: CreatableStatusReporterV2Zod.optional() })
+export const CreatableContextZod = CachingContextZod.extend({ statusReporter: CreatableStatusReporterV2Zod.optional() })
 
 export const CreatableParamsV2Zod = BaseEmitterParamsV2Zod.extend({ context: CreatableContextZod, name: z.string() })
 

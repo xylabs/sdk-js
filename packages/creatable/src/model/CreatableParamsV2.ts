3130a719c9b71bae29320bb1c2e25a0898dcfabc
Arie Trouw <arie.trouw@xylabs.com>
2026-02-09 14:59:44 -0800

    removed name from CreatableContext

--- packages/creatable/src/model/CreatableParamsV2.ts
+++ packages/creatable/src/model/CreatableParamsV2.ts
@@ -4,7 +4,7 @@ import z from 'zod'
 
 import { CreatableStatusReporterV2Zod } from './CreatableStatusReporter.zod.ts'
 
-export const CreatableContextZod = BaseContextZod.extend(z.object({ name: z.string(), statusReporter: CreatableStatusReporterV2Zod.optional() }).shape)
+export const CreatableContextZod = BaseContextZod.extend(z.object({ statusReporter: CreatableStatusReporterV2Zod.optional() }).shape)
 
 export const CreatableParamsV2Zod = BaseEmitterParamsV2Zod.extend(z.object({ context: CreatableContextZod, name: z.string() }).shape)
 
